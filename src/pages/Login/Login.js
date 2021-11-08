4fc4d7f195173a23382a942bd638c6c2ae5f6046
Jeffrey Liu <39445369+Causality-C@users.noreply.github.com>
2021-11-08 14:58:25 -0600

    Initial commit

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="main-container">
+      <h1>Login</h1>
+      <input
+        className="input"
+        placeholder="Username"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      ></input>
+      <input
+        className="input"
+        placeholder="Password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      ></input>
+      <button className="button">Login</button>
+      <Link className="signup-link" to="/signup">
+        Signup
+      </Link>
+    </div>
+  );
+}
